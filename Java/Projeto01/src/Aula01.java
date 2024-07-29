87aa835c51b3000f8749afb497f835012c345943
EduardoRigobeli <eduardo.souza@grupointegrado.br>
2024-07-29 20:30:39 -0300

    Estrutura de Dados

---
+++ Java/Projeto01/src/Aula01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Aula01 {
+    public static void main(String[] args) {
+        Scanner prompt = new Scanner(System.in);
+
+        String nome = prompt.next();
+        System.out.println("O seu nome é: "+nome);
+    }
+}
