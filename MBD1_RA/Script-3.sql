4878796975a23a2810402bdc97c3cb09db3d3c36
EduardoRigobeli <eduardo.souza@grupointegrado.br>
2024-09-04 17:20:09 -0300

    conexão com o MySql

---
+++ MBD1_RA/Script-3.sql
@@ -0,0 +1,39 @@
+/*EXERC 1*/
+select * from produto
+where UNIDADE = 'UN';
+
+/*EXERC 2*/
+select * from produto
+where CODIGO_CLASSIFICACAO = '020' and QUANTIDADE > 20;
+
+/*EXERC 3*/
+select * from produto
+where DESCRICAO like '%BALA%' and CODIGO_CLASSIFICACAO = '003' and QUANTIDADE > 6;
+
+/*EXERC 4*/
+select * from produto
+where DESCRICAO like '%martelo';
+
+/*EXERC 5*/
+select * from classificacao
+
+
+/*EXERC 6*/
+
+
+/*EXERC 7*/
+
+
+/*EXERC 8*/
+
+
+/*EXERC 10*/
+select * from produto 
+where QUANTIDADE > 6 and QUANTIDADE < 10;
+
+/*EXERC 11*/
+select * from produto 
+where DESCRICAO like '%oleo';
+
+/*EXERC 12*/
+
